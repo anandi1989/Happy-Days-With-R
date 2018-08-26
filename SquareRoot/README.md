67d6f031b5f1250a3f43c588b129d39b7cb38f7f
Amit Nandi <amit_nandi@live.com>
2018-08-27 00:21:05 +0530

    Add files via upload

---
+++ SquareRoot/README.md
@@ -0,0 +1,15 @@
+## Introduction
+
+The Babylonian method for computing the square root of a positive real number `S`: Start with any arbitrary <img src="https://latex.codecogs.com/gif.latex?x_0" title="x_0 > 0" /> , and iterate `(n =
+0, 1, ...)`
+
+<img src="https://latex.codecogs.com/gif.latex?x_{n&plus;1}&space;=&space;\frac{1}{2}(x_n&plus;\frac{S}{x_n})" title="x_{n+1} = \frac{1}{2}(x_n+\frac{S}{x_n})" />
+
+The Indian method for computing the square root of a positive real number `S`: Start with any arbitrary <img src="https://latex.codecogs.com/gif.latex?x_0" title="x_0 > 0" /> , and iterate `(n =
+0, 1, ...)`
+
+<img src="https://latex.codecogs.com/gif.latex?a_n&space;=&space;\frac{S&space;-&space;x_n^2}{2x_n}" title="a_n = \frac{S - x_n^2}{2x_n}" />
+
+<img src="https://latex.codecogs.com/gif.latex?x_{n&plus;1}&space;=&space;x_n&space;&plus;&space;a_n&space;&plus;&space;\frac{1}{2}\frac{a_n^2}{x_n&space;&plus;&space;a_n}" title="x_{n+1} = x_n + a_n + \frac{1}{2}\frac{a_n^2}{x_n + a_n}" />
+
+Plot the convergence rate of Babylonian and Indian method.
---
+++ SquareRoot/squareroot.r
@@ -0,0 +1,59 @@
+root<-function(x)
+{
+	epsilon<-1e-08 #conditon
+	R<-c()
+	R[1]<-10 #initial guess
+	count<-0
+	repeat       #operation using repeat function
+	{
+		R[2]=(R[1]+(x/R[1]))/2
+		if(abs(R[2]-R[1])<epsilon)
+			break
+		else
+			R[1]<-R[2]
+		count<-count+1 
+	}
+	#cat("Root of the number\n",R[2]) #output
+	#cat("\nNumber of iteration done:",count)
+	return(count)
+
+}
+
+
+indian_root<-function(y)
+{
+	pi<- 1e-08 #condition
+	N<-c()
+	N[1]<-10 #initial guess
+	A<-c()
+	counter<-0
+	repeat              #operation using 'repeat' function
+	{
+		A[1]<- (y-(N[1])^2)/(2*N[1])
+		N[2]<- N[1]+A[1]+(A[1])^2/(2*(N[1]+A[1]))
+		if(abs(N[2]-N[1])<pi)
+			break
+		else
+			N[1]<-N[2]
+		counter<-counter+1
+	}
+	#cat("\nRoot of the Number is: ",N[2]) #output
+	#cat("\nNumber of iteration: ",counter)
+	return(counter)
+}
+pdf('Comparison between Babylonian & Indian Method.pdf')
+#Compare between babylonian method & Indian method
+ind_iteration<-c()
+baby_iteration<-c()
+for(i in 1:100)
+{
+	ind_iteration[i]=indian_root(i)
+	baby_iteration[i]=root(i)
+}
+
+plot(ind_iteration,type='l',ylab="Number of Iteration in Babylonian & Indian Method", xlab = "Numbers",main="Comparison between Babylonian and Indian method")
+points(baby_iteration,type='l',col="red")
+
+legend('topright',c("Indian Method","Babylonian Method"),pch=20,col=c("Black","Red"),bg="white",ncol=2,cex=0.75)
+
+dev.off()
